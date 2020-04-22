858fa0ab6e063020cec8437e5b121012ef7a5912
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-21 23:36:59 -0500

    Create mealsMock.js

---
+++ server/mocks/mealsMock.js
@@ -0,0 +1,91 @@
+let meals = [
+    {
+        "id": 1,
+        "name": "Comida 1",
+        "description": "Laboris consectetur aliqua veniam eu sunt velit sint commodo id nulla irure amet officia.",
+        "price": 12000,
+        "images": [
+            "/assets/fruit-salad.jpg",
+            "/assets/fruit-salad-1.jpg"
+        ],
+        "ingredients": [
+            {
+                "name": "ingrediente1",
+                "quantity": 50
+            },
+            {
+                "name": "ingredient2",
+                "quantity": 50
+            },
+            {
+                "name": "Ingrediente 3",
+                "quantity": 50
+            }
+        ],
+        "calories": 50,
+        "fats": 5,
+        "carbs": 0,
+        "proteins": 0
+    },
+    {
+        "id": 2,
+        "name": "Comida 2",
+        "description": "Est amet sint esse sint voluptate fugiat magna nulla laboris nostrud pariatur laborum.",
+        "price": 15000,
+        "images": [
+            "/assets/salmon.jpeg",
+            "/assets/salmon-1.jpg"
+        ],
+        "ingredients": [
+            {
+                "name": "ingrediente1",
+                "quantity": 60
+            },
+            {
+                "name": "ingredient2",
+                "quantity": 35
+            },
+            {
+                "name": "Ingrediente 3",
+                "quantity": 100
+            }
+        ],
+        "calories": 250,
+        "fats": 5,
+        "carbs": 26,
+        "proteins": 150
+    },
+    {
+        "id": 3,
+        "name": "Comida 3",
+        "description": "Sint non non aliquip magna ad mollit amet aute deserunt ut.",
+        "price": 20000,
+        "images": [
+            "/assets/steak.jpg"
+        ],
+        "ingredients": [
+            {
+                "name": "ingrediente1",
+                "quantity": 40
+            },
+            {
+                "name": "ingredient2",
+                "quantity": 200
+            },
+            {
+                "name": "Ingrediente 3",
+                "quantity": 155
+            },
+            {
+                "name": "Ingrediente 4",
+                "quantity": 300
+            }
+        ],
+        "calories": 235,
+        "fats": 30,
+        "proteins": 200,
+        "carbs": 0
+    }
+]
+
+export default meals;
