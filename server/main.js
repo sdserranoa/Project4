863d13a2c3943b3f7688f640f7a3a4a84aa7a2b1
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-21 23:37:12 -0500

    Added meals mock to db

--- server/main.js
+++ server/main.js
@@ -1,6 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { Orders } from '../imports/api/orders.js';
+import { Meals } from '../imports/api/meals';
+import mealsMock from './mocks/mealsMock';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meals.remove({})
+  console.log(Meals.find({}).count())
+  if (Meals.find({}).count() === 0) {
+    mealsMock.forEach(e => {
+      Meals.insert(e)
+    })
+  }
 });
