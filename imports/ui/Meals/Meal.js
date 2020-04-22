70459f558655d826173c128d90ea7fd9b5a0f7ab
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-21 23:37:35 -0500

    Update Meal.js

--- imports/ui/Meals/Meal.js
+++ imports/ui/Meals/Meal.js
@@ -1,41 +1,38 @@
 import React, { Component } from 'react'
-import { Button, Image, Container, Row, Col } from 'react-bootstrap'
+import { Image, Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
-/* ICONS */
-import CartIcon from '@material-ui/icons/AddShoppingCartRounded';
+import CartButton from '../cart/CartButton'
+
 
 export default class Meal extends Component {
     render() {
         let meal = this.props.meal
 
         return (
-            <div className="meal-card">
-                <Container fluid>
-                    <Row className="justify-content-md-center">
-                        <Col lg={3}>
-                            <Image className="meal-thumbnail" src={meal.images[0]} roundedCircle />
-                        </Col>
+            <Link to={"/meals/" + meal.id} className="card-link">
+                < div className="meal-card">
+                    <Container fluid>
+                        <Row className="justify-content-md-center align-items-center">
+                            <Col lg={3} className="text-center">
+                                <Image className="meal-thumbnail" src={meal.images[0]} roundedCircle />
+                            </Col>
 
-                        <Col>
-                            <Row as="h3">{meal.name}</Row>
-                            <Row as="p">{meal.description}</Row>
-                            <Row as="p" style={{ color: 'green' }}>
-                                <b>${meal.price}</b>
-                            </Row>
-                            <Row>
-                                <Button
-                                    variant="outline-primary"
-                                    style={{
-                                        marginRight: '15px',
-                                        marginLeft: 'auto'
-                                    }}>
-                                    Add to Cart <CartIcon />
-                                </Button>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                            <Col>
+                                <Row as="h3">{meal.name}</Row>
+                                <Row as="h5" style={{ color: 'grey' }}>Calorías: {meal.calories.quantity}</Row>
+                                <Row as="p">{meal.description}</Row>
+                                <Row as="p" style={{ color: 'green' }}>
+                                    <b>${meal.price}</b>
+                                </Row>
+                                <Row>
+                                    <CartButton />
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div >
+            </Link >
         )
     }
 }
