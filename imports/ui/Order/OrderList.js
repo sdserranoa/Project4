0159c7b42962da2e53d14bd6b0e59b76f1bd504b
CarlosOrduz <47404630+CarlosOrduz@users.noreply.github.com>
2020-04-22 20:28:17 -0500

    Search filter en ordenes

--- imports/ui/Order/OrderList.js
+++ imports/ui/Order/OrderList.js
@@ -7,46 +7,47 @@ export default class OrderList extends Component {
 
     state = {
         orders: [
-            {
-                id:1,
-                menu:'Carne',
-                price:'100',
-                state:'Preparation',
-                addres:'calle2'
-            },
-            {
-                id:2,
-                menu:'Carne1',
-                price:'100',
-                state:'Preparation',
-                addres:'calle2'
-            },
-            {
-                id:3,
-                menu:'Carne2',
-                price:'100',
-                state:'Preparation',
-                addres:'calle2'
-            },
-            {
-                id:4,
-                menu:'Carne3',
-                price:'100',
-                state:'Preparation',
-                addres:'calle2'
-            }
-        ]
+            this.createData('Cupcake', 305, 3.7, 67, 4.3),
+            this. createData('Donut', 452, 25.0, 51, 4.9),
+            this.createData('Eclair', 262, 16.0, 24, 6.0),
+            this.createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+            this.createData('Gingerbread', 356, 16.0, 49, 3.9),
+            this. createData('Honeycomb', 408, 3.2, 87, 6.5),
+            this.createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+            this.createData('Jelly Bean', 375, 0.0, 94, 0.0),
+            this.createData('KitKat', 518, 26.0, 65, 7.0),
+            this.createData('Lollipop', 392, 0.2, 98, 0.0),
+            this. createData('Marshmallow', 318, 0, 81, 2.0),
+            this.createData('Nougat', 360, 19.0, 9, 37.0),
+            this.createData('Oreo', 437, 18.0, 63, 4.0),
+        ],
+        inputValue:''
     }
-
+    createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+      }
+      orderFilterOnChange=(event)=>{
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
     render() {
+        
+        const filteredOrders=
+            this.state.orders.filter(order=>{
+                
+                return order.name.toLowerCase().includes(this.state.inputValue.toLowerCase())
+            })
+    
         return (
             <div>
                 <Container fluid="true">
                     <h1 style={{ textAlign: "center" }}>Orders</h1>
                     <hr></hr>
+                    <input type="text" id="min" name="min" placeholder="Search" value={this.state.inputValue} onChange={this.orderFilterOnChange}></input>
                     <Row>
                         <Col>
-                            <EnhancedTable/>
+                            <EnhancedTable rows={filteredOrders}/>
                         </Col>
                     </Row>
                 </Container>
