f76537cf23c5c7d4937ad7c9ec8f8a4ee9c529cd
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-22 20:16:59 -0500

    Added filters to meals

--- imports/ui/Index.js
+++ imports/ui/Index.js
@@ -10,37 +10,38 @@ import Orders from './Order/OrderList.js';
 import Meals from './Meals/MealList';
 import MealDetail from './Meals/MealDetail';
 import { Meteor } from 'meteor/meteor';
+import MealFilter from './Meals/MealFilter.js';
 
 
 
 class Index extends Component {
     constructor() {
         super();
-        this.state = {isAuthenticated: Meteor.userId() !== null};
+        this.state = { isAuthenticated: Meteor.userId() !== null };
         //this.getMeteorData=this.getMeteorData.bind(this);
-        this.login=this.login.bind(this);
-        this.logout=this.logout.bind(this);
-        this.singup=this.singup.bind(this);
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+        this.singup = this.singup.bind(this);
     }
 
-    getMeteorData= () => {
+    getMeteorData = () => {
         this.setState({ isAuthenticated: Meteor.userId() !== null });
     }
 
-    login(username, password){
-        
+    login(username, password) {
+
         Meteor.loginWithPassword(username, password, (err) => {
             if (err) {
                 console.log(err.reason);
             } else {
-               console.log("this.props.history.push('/');"); 
+                console.log("this.props.history.push('/');");
             }
             this.getMeteorData();
         });
-        
-      }
 
-      logout() {
+    }
+
+    logout() {
         Meteor.logout((err) => {
             if (err) {
                 console.log(err.reason);
@@ -49,35 +50,35 @@ class Index extends Component {
             }
             this.getMeteorData();
         });
-        
+
     }
-    singup(email, username, password){
-        Accounts.createUser({email, username, password}, (err) => {
-            if(err){
+    singup(email, username, password) {
+        Accounts.createUser({ email, username, password }, (err) => {
+            if (err) {
                 console.log(err.reason);
                 this.showValidationErr("email", "That's an incorrect email!");
             } else {
-              console.log("this.props.history.push('/login');");
+                console.log("this.props.history.push('/login');");
             }
             this.getMeteorData();
-          });
-          
+        });
+
     }
 
 
     render() {
         return (
             <Router>
-                <Navigation logout={this.logout} usid={this.state.isAuthenticated}/>
+                <Navigation logout={this.logout} usid={this.state.isAuthenticated} />
                 <Route path="/" exact component={Home} />
                 <Route path="/restaurant" component={Restaurants} />
                 <Route path="/diets" component={Diets} />
                 <Route path="/contact" component={Contact} />
                 <Route path="/orders" component={Orders} />
-                <Route exact path="/meals" component={Meals} />
+                <Route exact path="/meals" component={MealFilter} />
                 <Route path="/meals/:id" component={MealDetail} />
 
-                <Route path="/SignUp" render={props => <UserWrap singup={ this.singup} login={this.login} />} />
+                <Route path="/SignUp" render={props => <UserWrap singup={this.singup} login={this.login} />} />
             </Router>
         );
     };
