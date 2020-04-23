47f74c354d225ee6e4fc50c405b972b1c8976fb6
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-22 23:49:14 -0500

    Noticaciones AL FIN!

--- imports/ui/cart/CartButton.js
+++ imports/ui/cart/CartButton.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 /* ICONS */
 import CartIcon from '@material-ui/icons/AddShoppingCartRounded';
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 export default class CartButton extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            showAlert: false
+        }
+    }
+
+    handleCloseAlert() {
+        this.setState({
+            showAlert: false
+        })
+    }
+
+    handleClick() {
+        this.setState({
+            showAlert: true
+        })
+    }
+
     render() {
         return (
-            <Button
-                variant="outline-primary"
-                style={{
-                    marginRight: '15px',
-                    marginLeft: 'auto'
-                }}>
-                Add to Cart <CartIcon />
-            </Button>
+            <div>
+                <Button
+                    variant="outline-primary"
+                    style={{
+                        marginRight: '15px',
+                        marginLeft: 'auto'
+                    }}
+                    onClick={this.handleClick.bind(this)}>
+                    Add to Cart <CartIcon />
+                </Button>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    open={this.state.showAlert}
+                    autoHideDuration={6000}
+                    onClose={this.handleCloseAlert.bind(this)}>
+                    <Alert onClose={this.handleCloseAlert.bind(this)} severity="success">
+                        ¡{this.props.mealName} agregado al carrito!
+                    </Alert>
+                </Snackbar>
+            </div>
         )
     }
 }
