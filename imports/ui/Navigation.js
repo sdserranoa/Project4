24a2fca5c3fbd401f4c0df1fe40ef170f785aa1d
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-24 17:00:40 -0500

    Navbar fixed

--- imports/ui/Navigation.js
+++ imports/ui/Navigation.js
@@ -18,12 +18,12 @@ export default class Navigation extends Component {
 
     constructor(props) {
         super(props);
-        this.state={}
+        this.state = {}
         //this.state={logoutON:this.props.isAuthenticated}
     }
     logoutReload(e) {
         console.log('entra a logoutReload');
-        this.props.logout(); 
+        this.props.logout();
         //this.t1();
         //<ToastU title={"You logged out succesfully!"} description={"Congrats! This is the first step into a healthy lifestyle!"} />
     }
@@ -38,8 +38,8 @@ export default class Navigation extends Component {
             })
     }
 */
-    
-render() {
+
+    render() {
         return (
             <div>
                 <Navbar bg="light" expand="lg">
@@ -47,13 +47,13 @@ render() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link><Link to="/" className="link">Home</Link></Nav.Link>
-                            <Nav.Link><Link to="/diets">Diets</Link></Nav.Link>
-                            <Nav.Link><Link to="/Orders">Orders</Link></Nav.Link>
-                            <Nav.Link><Link to="/meals">Meals</Link></Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/diets">Diets</Nav.Link>
+                            <Nav.Link href="/Orders">Orders</Nav.Link>
+                            <Nav.Link href="/meals">Meals</Nav.Link>
                             {Meteor.userId() &&
-                            <Nav.Link><Link to="/chat">Chat</Link></Nav.Link>
-                             }
+                                <Nav.Link href="/chat">Chat</Nav.Link>
+                            }
                         </Nav>
                         <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -64,20 +64,20 @@ render() {
                                 <Nav.Link ><Link to="/SignUp" id="SignUpLink" variant="dark">Sign up</Link>
                                 </Nav.Link>
                             }
-                            
+
                             {Meteor.userId() &&
                                 <Nav.Link ><Link to="/" onClick={this.logoutReload.bind(this)} id="SignUpLink" variant="dark" >Log out </Link>
                                 </Nav.Link>
                             }
-  
+
                             {Meteor.userId() &&
                                 <Nav.Link ><Link to="/" id="currentUsername" >{/*Meteor.user().username[0].toUpperCase()+Meteor.user().username.slice(1).toLowerCase()*/}</Link>
-                                    </Nav.Link>
+                                </Nav.Link>
                             }
 
                         </Nav>
                     </Navbar.Collapse>
-                </Navbar> 
+                </Navbar>
             </div>
         );
     };
